f681e4e8ee36b2742a7f15579206b71ff01b8745
Abdullah ikram ghauri <147640702+abdullahikram90@users.noreply.github.com>
2025-08-09 12:07:35 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,11 @@
+let score = "332"
+console.log(score)
+
+console.log(typeof score);
+
+let valueInNmuber = Number(score)
+console.log(typeof valueInNmuber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1 ; false=> 0
