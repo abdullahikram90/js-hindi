59b896a403693fa43920dc3e3e28c98127b186f5
Abdullah ikram ghauri <147640702+abdullahikram90@users.noreply.github.com>
2025-08-09 11:16:45 +0000

    change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_Id= 12233
+let accountEmail = "abdullahghauri89@gmail.com"
+var accountPasword = "1234"
+accountCity = "multan"
+
+accountEmail = "asdf@gmail.com"
+accountPasword = "1234"
+accountCity = "lahore"
+
+console.log(account_Id);
+
+
+console.table([account_Id,accountEmail,accountPasword,accountCity])
+    
