da7a5b7df2903141aecc63f2fd2a881e3a9e03c2
Abdullah ikram ghauri <147640702+abdullahikram90@users.noreply.github.com>
2025-08-10 08:22:47 +0000

    icrement operator

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -10,3 +10,18 @@ console.log(typeof valueInNmuber);
 // "33abc" => NaN
 //  true => 1 ; false=> 0
 
+
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " abdullah"
+let str3 = str1 + str2
+console.log(str3);
+
+// increment operator
+
+let x = 3
+const y = x++;
+console.log(x);
