47d1353cf0110ae4ca5e12ba5918adb4eef1e4d6
abdullahikram90 <abdullahghauri90@gmail.com>
2025-08-12 12:54:13 +0500

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+const myarray = [1, 2, 3, 4, 5, "ABDULLAH"]
+console.log(myarray[0]);
+
+const myArr = new Array(1, 2, 3, 4, 5,)
+console.log(myArr[1]);
+
+
+
+myArr.push(6)
+console.log(myArr);
---
+++ 02_basics/02_array.js
@@ -0,0 +1,8 @@
+const marvelhero = ["spiderman", "ironman", "thor"]
+console.log(marvelhero);
+
+const pkhero = ["salman", "atif", "ghayas" ]
+console.log(pkhero);
+
+const allheros = marvelhero.concat(pkhero)
+console.log(allheros);
